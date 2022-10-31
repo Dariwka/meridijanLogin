08e0f65fc104f0b3c3d18802bd3405a07a119f2d
Dariwka <77488950+Dariwka@users.noreply.github.com>
2022-10-31 15:26:40 +0200

    added personal page as home page, empty signup page

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Login from "./components/Login";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Login />
+    <Home />
   </React.StrictMode>
 );
 
